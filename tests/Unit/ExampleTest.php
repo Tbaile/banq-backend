ad909071c669511c46b6fc03712805336f6bd2f3
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2023-02-06 15:31:29 +0100

    feat(deps): moved testing to pest

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,18 +1,8 @@
 <?php
 
-namespace Tests\Unit;
-
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_that_true_is_true()
-    {
-        $this->assertTrue(true);
-    }
-}
+/**
+ * A basic test example.
+ */
+test('true is true')
+    ->expect(true)
+    ->toBeTrue();
