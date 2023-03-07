122759cde6d37e9db32e6e1adf63e398396838df
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2023-03-07 15:17:06 +0100

    refactor(build): improved bot dev and deploy images

--- containers/php/entrypoint.sh
+++ containers/php/entrypoint.sh
@@ -9,7 +9,11 @@ else
     if [ "$ROLE" = "app" ]; then
         exec php-fpm
     elif [ "$ROLE" = "setup" ]; then
-        php artisan app:setup
+        php artisan config:cache
+        php artisan view:cache
+        php artisan storage:link
+        php artisan migrate --force
+        chown -R www-data:www-data storage
     else
         echo "Unknown role '$ROLE'"
         exit 1
