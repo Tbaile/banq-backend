cc18f6c98c7026e2a0ac6c4d66e4d35e2d80835b
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2023-03-09 17:34:27 +0100

    feat: added Tag

---
+++ app/Models/Tag.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Tag extends Model
+{
+    use HasFactory;
+
+    /**
+     * Allows laravel to associate tags to every model is needed.
+     */
+    public function taggable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+}
