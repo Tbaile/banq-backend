0126aa7a20b1407e8ee93691a406daecd3372f20
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2023-04-11 14:12:12 +0200

    build(web): updated container
    Removed entrypoint and added wait-for in /docker-entrypoint.d
    Updated H5PB configuration

--- containers/nginx/entrypoint.sh
+++ containers/nginx/entrypoint.sh
@@ -2,7 +2,4 @@
 
 set -e
 
-if [ "$1" = 'nginx' ]; then
-    wait-for "${FPM_URL:?Missing FPM_URL}:${FPM_PORT:?Missing FPM_PORT}" -t 60
-fi
-exec /docker-entrypoint.sh "$@"
+wait-for "${FPM_URL:?Missing FPM_URL}:${FPM_PORT:?Missing FPM_PORT}" -t 60
