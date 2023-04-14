aa424c4d5132032bc9161ebcf73cc0093e4b8cff
Tommaso Bailetti <bailetti.tommaso@gmail.com>
2023-04-14 17:23:07 +0200

    feat(assets): added user factory to asset factory

--- database/factories/AssetFactory.php
+++ database/factories/AssetFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Enum\CurrencyEnum;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -20,6 +21,7 @@ class AssetFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'currency' => $this->faker->randomElement(CurrencyEnum::cases()),
+            'user_id' => User::factory(),
         ];
     }
 }
